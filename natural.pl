f1d5ba00210d32dfce09d81fa5293cb70560ca6d
PabloMendez3476 <pablo.desousa@alu.uhu.es>
2025-02-21 11:16:38 +0100

    Create natural.pl

---
+++ natural.pl
@@ -0,0 +1,30 @@
+
+
+
+/*
+
+ratural(N)
+	es cierto si N es un número natural.
+	
+Principio de Inducción Matemática
+
+	Queremos edmostrar que una propiedad P es cierta para todos 
+	los elementos del conjunto S. Precondición: 
+	Los elementod de S tienen que ser ordenables.
+	(Tendré un elemento n0 que será el más pequeño).
+	
+1) F(n0) es cierto.
+
+2) Para todo n > n0, Si (Pn-1) es cierta, entonces P(n).
+
+   Si P(n) es cierta, entonces P(n+1).
+*/
+
+natural(1).
+natural(N):- N2 is N-1, natural(N-1).
+
+/*
+ En prolog le damos la vuelta a las reglas.
+ A -> B
+ B :- A.
+*/
